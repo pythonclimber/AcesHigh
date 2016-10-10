882f5b424f62aaf695a688a2bd719ec75ba4f278
rubyclimber <asmitty92@gmail.com>
2016-10-10 11:33:56 -0500

    Add test app

---
+++ CardTestApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using CardFramework;
+using CardFramework.Decks;
+
+namespace CardTestApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
