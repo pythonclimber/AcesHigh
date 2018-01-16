e7456400f5f0ff7d93d303bbf092a1bea7995316
Aaron Smith <asmitty92@gmail.com>
2018-01-16 12:12:39 -0600

    Add CardCombination.cs file

---
+++ CardFramework/CardFramework/Decks/CardCombination.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CardFramework.Decks {
+    public class CardCombination {
+        private List<Card> _cards;
+        private int _score;
+
+        public CardCombination(List<Card> cards, int score) {
+            _cards = cards;
+            _score = score;
+        }
+    }
+}
